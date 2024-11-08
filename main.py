0349615972b5236ed8d8dad692228f9a54907b1b
mees van Dijk <meesvandijk@mijnheem.nl>
2024-11-08 10:55:27 +0100

    Laatste commit voor PWS, nog niet multithreaded

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import json
 from fastapi import FastAPI, File, UploadFile, Request
 from fastapi.middleware.cors import CORSMiddleware
-# from ultralytics import YOLO
+from fastapi.responses import StreamingResponse
+from fastapi.staticfiles import StaticFiles
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from deepface import DeepFace
@@ -9,7 +10,6 @@ import pandas as pd
 import logging
 import base64
 import io
-from fastapi.responses import StreamingResponse
 from picamera2 import Picamera2, Preview
 import time
 import asyncio
@@ -21,15 +21,17 @@ from datetime import datetime
 
 app = FastAPI()
 executor = ThreadPoolExecutor(max_workers=5)
-picam2 = None  # Global camera instance
-camera_lock = Lock()  # Lock to ensure thread safety
+picam2 = None
+camera_lock = Lock()
 
+app.mount("/herkend", StaticFiles(directory="herkend"), name="herkend")
 # CORS setup
 origins = [
     "http://localhost:8000", 
     "http://127.0.0.1:8000",
     "http://localhost:5000",
     "http://localhost:3000",
+    "*"
 
 ]
 app.add_middleware(
@@ -44,29 +46,16 @@ app.add_middleware(
 async def startup_event():
     global picam2
     picam2 = Picamera2()
-    picam2.start()  # Start the camera
+    picam2.configure(picam2.create_preview_configuration())
+    picam2.start()
+
 
 @app.on_event("shutdown")
 async def shutdown_event():
     global picam2
     if picam2:
-        picam2.stop()  # Stop the camera
+        picam2.stop()
 
-
-
-# @app.get("/img")
-# async def img():
-#     if not picam2:
-#         raise HTTPException(status_code=500, detail="Camera not initialized.")
-
-#     with camera_lock: 
-#         try:
-#             loop = asyncio.get_running_loop()
-#             Buffer = await loop.run_in_executor(executor, capture_image)
-#             recognize = await recognize_image(Buffer)
-#             return StreamingResponse(recognize, media_type="image/jpeg")
-#         except Exception as e:
-#             raise HTTPException(status_code=500, detail=str(e))
 
 @app.post("/predict/")
 async def predict(request: Request):
@@ -78,29 +67,20 @@ async def predict(request: Request):
 
     return {"newBuffer": newBuffer}
 
-    
-# def capture_image():
-#     stream = io.BytesIO()
-#     picam2.capture_file(stream, format="jpeg")
-#     stream.seek(0)
-#     image = Image.open(stream)
-#     return image
-
-async def generate_frames():
-    while True:
-        await asyncio.sleep(0.01)
-        with camera_lock:
-            stream = io.BytesIO()
-            picam2.capture_file(stream, format="jpeg")
-            stream.seek(0)
-
-            yield (b'--frame\r\n'
-                    b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
-
 @app.get("/video")
 async def video_stream():
     if not picam2:
         raise HTTPException(status_code=500, detail="Camera not initialized.")
+    async def generate_frames():
+        while True:
+            await asyncio.sleep(0.01)
+            with camera_lock:
+                stream = io.BytesIO()
+                picam2.capture_file(stream, format="jpeg")
+                stream.seek(0)
+
+                yield (b'--frame\r\n'
+                        b'Content-Type: image/jpeg\r\n\r\n' + stream.read() + b'\r\n')
 
     return StreamingResponse(generate_frames(), media_type="multipart/x-mixed-replace; boundary=frame")
 
@@ -117,9 +97,9 @@ async def recognize(request: Request):
         picam2.capture_file(stream, format="jpeg")
         stream.seek(0)
         print("going to cal lrecognize")
-        await recognize_image(stream)
+        Result = await recognize_image(stream)
     
-    return {"status": "Frame captured and saved"}
+    return Result
         
 
 
@@ -127,30 +107,22 @@ async def recognize_image(stream):
     print("gonna recognize")
     image = Image.open(stream)
     imgArray = np.array(image)
+    print("Now going to run DeepFace")
     results = DeepFace.find(img_path=imgArray, db_path="images",  enforce_detection=False)  
-    print("Result: ",results, "end")
+    print("Result: ",results)
     if isinstance(results, list) and len(results) > 0:
         json_results = json.dumps([result.to_dict() if isinstance(result, pd.DataFrame) else result for result in results])
     else:
         json_results = json.dumps(results)
 
-    print("JSON: ", json_results)
     
-    with open('result.json', 'w') as f:
-        f.write(json_results)
-    print("DID JSON")
-
-    with open('result.txt', 'w') as f:
-        for item in results:
-            f.write("%s\n" % item)
-
     draw = ImageDraw.Draw(image)
+
     try:
         font = ImageFont.truetype("SUSE-Regular.ttf", 40)
     except IOError:
         font = None
 
-    print(results)
     for person in results:
         if len(person["target_x"]) == 0:
             continue
@@ -164,9 +136,8 @@ async def recognize_image(stream):
 
     buffered = io.BytesIO()
     image.save(buffered, format="JPEG")
-    with open("output_image.jpg", "wb") as f:  # 'wb' means write in binary mode
+    FileName = f"herkend/herkend_op_{datetime.now().strftime('%d-%m-%Y-%H:%M:%S')}.jpg"
+    with open(FileName, "wb") as f: 
         f.write(buffered.getvalue())
-    base64_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
 
-
-    return base64_image  
+    return FileName
